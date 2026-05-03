065e196ec7c7ba9320f7097658aef1be206b5f8d
Michelle Okolie <michelleokolie11@gmail.com>
2026-05-03 13:45:37 -0500

    chore: added commas and changed quotes

--- database/04_create_friendships.sql
+++ database/04_create_friendships.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS public.friendships (
     user_id UUID REFERENCES public.profiles(id) ON DELETE CASCADE,
     friend_id UUID REFERENCES public.profiles(id) ON DELETE CASCADE,
-    status TEXT NOT NULL DEFAULT "pending",
+    status TEXT NOT NULL DEFAULT 'pending',
     created_at TIMESTAMPTZ DEFAULT NOW(),
     PRIMARY KEY(user_id, friend_id),
-    CONSTRAINT different_users CHECK (user_id <> friend_id)
+    CONSTRAINT different_users CHECK (user_id <> friend_id),
     -- Need a way to not double probe later on, so user_id is less than friend id to avoid duplicate rows
     CONSTRAINT id_order CHECK (user_id < friend_id)
 );
