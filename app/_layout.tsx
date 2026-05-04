0ac7ffaa59eea1c8d81298f1f16afbb7e9cfcb3e
Michelle Okolie <michelleokolie11@gmail.com>
2026-05-04 13:03:08 -0500

    chore: added use effect for auth init

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,20 +7,42 @@
 // 3. Tell Supabase that if login state changes, let us know
 // 4. Based on state, send to auth or tabs
 
-import { useEffect, useState } from "react";
-import { Slot, useRouter } from "expo-router";
-import { supabase } from "../src/lib/supabase";
+import { supabase } from "@/src/lib/supabase";
 import { Session } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
 
 export default function RootLayout() {
-  const [currentSession, setCurrentSession] = useState(null);
+  const [currentSession, setCurrentSession] = useState<Session | null>(null);
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {}, [currentSession]);
+  // This useEffect is for all the init. related to auth
+  useEffect(() => {
+    // 1. ask supabase for the current session and store it
+    const init = async () => {
+      // getSession returns obj. with keys data and error
+      // docs say to consider getClaims or getUser, review that
+      // 2. tell supabase to watch for auth changes and update our state when it does
+      // 3. set isLoading to false when we're done
+      const { data } = await supabase.auth.getSession();
+      setCurrentSession(data.session);
+      setIsLoading(false);
+    };
+    init();
 
-  return (
-    <>
-      <p>Test</p>
-    </>
-  );
+    const { data } = supabase.auth.onAuthStateChange((event, session) => {
+      if (event == "SIGNED_OUT") {
+        setCurrentSession(null);
+      } else {
+        setCurrentSession(session);
+      }
+    });
+
+    return () => {
+      data.subscription.unsubscribe();
+    };
+  }, []);
+
+  // This useEffect is for all nav related stuff for now
+  useEffect(() => {}, []);
+  return <></>;
 }
