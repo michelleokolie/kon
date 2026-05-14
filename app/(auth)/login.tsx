f098c02a1a93919afa338a6a26f44d3e88bee83b
Michelle Okolie <michelleokolie11@gmail.com>
2026-05-13 23:05:50 -0500

    chore: added the concepts of a plan

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,9 +1,35 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
-export default function Page() {
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // To deal with possible errors
+  const [error, setError] = useState<string | null>(null);
+
+  // This function will be called when the submit button is pressed
+  // It will call a supabase defined function and pass the user's info to that func.
+  const handleLogin = () => {
+    // We need to validate our inputs
+    // Then we need to see if the account exists already or not (irrelevant, supabase tells us this through error)
+    // Then we can pass to supabase
+  };
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Placeholder</Text>
+      <TextInput value={email} onChangeText={(text) => setEmail(text)} />
+      <TextInput
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+        secureTextEntry
+      />
+
+      <TouchableOpacity onPress={handleLogin}>
+        <Text>Login</Text>
+      </TouchableOpacity>
+
+      {error && <Text>{error}</Text>}
     </View>
   );
 }
